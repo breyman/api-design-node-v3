62cb7287280abf4c76d5d8a12d2953d8585b20b6
Brian Reyman <>
2022-09-15 18:18:01 -0600

    Brians Learning Updates

--- src/server.js
+++ src/server.js
@@ -12,4 +12,19 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World!' });
+}
+);
+
+app.post('/', (req, res) => {
+   console.log(req.body); 
+   res.send({ message: 'post received' });
+}
+);
+
+export const start = () => {
+    app.listen(3000, () => {
+        console.log('Server is listening on port 3000');
+    })
+}
